8f2c303dc084fdcb666e6747ea64708e620092b6
G00407095@atu.ie <G00407095@atu.ie>
2025-04-08 09:34:12 +0100

    added simple success test for first name

---
+++ src/main/java/ie/atu/exam/Customer.java
@@ -0,0 +1,10 @@
+package ie.atu.exam;
+
+public class Customer {
+    public Customer() {
+    }
+    public String addFirstName(String fName){
+
+        return fName;
+    }
+}
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,8 +1,15 @@
+import ie.atu.exam.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerTest {
 
+    @BeforeEach
+    void setup(){
+        Customer customer = new Customer();
+    }
     @Test
     void testFirstName(){
         Customer customer = new Customer();
