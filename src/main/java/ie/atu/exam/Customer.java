013b9221aac86a5b4017eb9d8b328da9ae598dda
G00407095@atu.ie <G00407095@atu.ie>
2025-04-08 09:43:13 +0100

    created success an failure tests for first name. the failure test doesnt work as i cant figure out how to throw the exception type. will continue and come back to it

--- src/main/java/ie/atu/exam/Customer.java
+++ src/main/java/ie/atu/exam/Customer.java
@@ -3,8 +3,10 @@ package ie.atu.exam;
 public class Customer {
     public Customer() {
     }
-    public String addFirstName(String fName){
-
+    public String addFirstName(String fName) throws Exception{
+        if(fName.length()< 3){
+            throw new Exception("must be > 3 chars");
+        }
         return fName;
     }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,8 +1,10 @@
 import ie.atu.exam.Customer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CustomerTest {
 
@@ -11,8 +13,16 @@ public class CustomerTest {
         Customer customer = new Customer();
     }
     @Test
-    void testFirstName(){
+    void testFirstName() throws Exception{
         Customer customer = new Customer();
         assertEquals("John" , customer.addFirstName("John"));
+    }
+    @Test
+    void testFirstNameFail(){
+        Customer customer = new Customer();
+        Exception e = assertThrows(Exception.class, () -> {
+            customer.addFirstName("bad");
+        });
+        assertEquals("must be > 3 chars", e.getMessage());
     }
 }
